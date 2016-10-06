4c9d5e17ef6584bbdaa7afcdcab4dfc53cf8172d
Ethan Fertsch <efertsch@gmail.com>
2016-10-06 10:11:33 -0700

    Initial commit, add questions to test whether emploee is a vampire

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts "Welcome to Our New Employee Vampire Detection Program"
+
+puts "What is your name?"
+employee_name = gets.chomp
+
+puts "What is your age?"
+employee_age = gets.to_i
+
+puts "What is your year of birth?"
+employee_yob = gets.to_i
+
+puts "Would you like us to order you some garlic bread from our cafeteria?"
+employee_garlic_response = gets.chomp
+
+puts "Would you like to enroll in the company health insurance program?"
+employee_health = gets.chomp
