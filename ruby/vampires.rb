f5f7c7cb4dc535a2cbb1910eb00df62f447296c3
Ethan Fertsch <efertsch@gmail.com>
2016-10-08 14:08:56 -0700

    Complete allergy checker and add to conditionals, tested all possible inputs to verify

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,15 +7,13 @@ puts "How many employees will be processed at this time?"
 number_of_employees = gets.to_i
 
 #Initialize loop
-
 until number_of_employees == 0
 
 #Employee name input, employee name flagged if 'Drake Cula' or 'Tu Fang'
-
 puts "What is your name?"
 employee_name = gets.chomp
 
-if employee_name == "DrakeCula"  #Cannot get this to work
+if employee_name == "DrakeCula"  
 	employee_name = false 
 elsif employee_name == "TuFang"
 	employee_name = false 
@@ -79,79 +77,60 @@ end
 	end
 
 #Employee allergy input, checks for and flags sunshine 	
-# puts "Please list any and all allergies you may have, type 'done' when you have finished."
-# employee_allergies = gets.chomp
-# until employee_allergies == "done" || employee_allergies == "sunshine"
-# 	puts "please enter another allergy."	
-# 	employee_allergies = gets.chomp 
-# end 
-# 	if employee_allergies == "sunshine"
-# 		employee_allergies = false
-# 	else
-# 		employee_allergies = true
-# 	end 
-# p employee_allergies
+puts "Please list any and all allergies you may have, type 'done' when you have finished."
+employee_allergies = gets.chomp
+until employee_allergies == "done" || employee_allergies == "sunshine"
+	puts "please enter another allergy."	
+	employee_allergies = gets.chomp 
+end 
+	if employee_allergies == "sunshine"
+		employee_allergies = false
+	else
+		employee_allergies = true
+	end 
+p employee_allergies
 
 puts
 puts "Results of Test:"
 puts
 
 
-if (employee_name && employee_age && employee_garlic_response) || (employee_name && employee_age && employee_health)  
+if (employee_name && employee_age && employee_garlic_response && employee_allergies) || (employee_name && employee_age && employee_health && employee_allergies)  
 	puts "Probably not a vampire"
-elsif employee_name && ( !employee_age && employee_garlic_response)
+elsif employee_name && ( !employee_age && employee_garlic_response) && employee_allergies
 	puts "Probably a vampire"
-elsif employee_name && ( !employee_age && employee_health)
+elsif employee_name && ( !employee_age && employee_health) && employee_allergies
 	puts "Probably a vampire"
-elsif employee_name && ( !employee_age && !employee_garlic_response && !employee_health)
+elsif employee_name && ( !employee_age && !employee_garlic_response && !employee_health) && employee_allergies
 	puts "Almost certainly a vampire"
-elsif !employee_name
+elsif !employee_name || !employee_allergies
 	puts "Definitely a vampire"
 else 
 	puts "Result inconclusive"
 end 
 
+# if (employee_name && employee_age && employee_garlic_response) || (employee_name && employee_age && employee_health)  
+# 	puts "Probably not a vampire"
+# elsif employee_name && ( !employee_age && employee_garlic_response)
+# 	puts "Probably a vampire"
+# elsif employee_name && ( !employee_age && employee_health)
+# 	puts "Probably a vampire"
+# elsif employee_name && ( !employee_age && !employee_garlic_response && !employee_health)
+# 	puts "Almost certainly a vampire"
+# elsif !employee_name || !employee_allergies
+# 	puts "Definitely a vampire"
+# else 
+# 	puts "Result inconclusive"
+# end 
 
 	
-#Conditional statements comparing results and returning output based on if employee age is valid
-# if employee_age == true then
-# 	if (employee_garlic_response == true  || employee_health == false) 
-# 		puts "Probably not a vampire."
-# 	elsif (employee_garlic_response == false || employee_health  == true)
-# 		puts "Probably not a vampire."
-# 	elsif employee_name == false 
-# 		puts "Definitely a vampire." 
-# 	elsif employee_allergies == false  
-# 		puts "Definitely a vampire"
-# 	else 
-# 		puts "Result inconclusive."
-# 	end 
-# end 	
-
-# #Conditional statements comparing results and returning output based on if employee age is not valid
-# 	if employee_age == false then 
-# 		if (employee_garlic_response == true || employee_health == false) 
-# 		puts "Probably a vampire."
-# 		elsif (employee_garlic_response == false || employee_health == true) 
-# 		puts "Probably a vampire."
-# 		elsif (employee_garlic_response == false || employee_health == false) 
-# 		puts "Almost certainly a vampire." 
-# 		elsif employee_name == false 
-# 		puts "Definitely a vampire." 
-# 		elsif employee_allergies == false 
-# 		puts "Definitely a vampire"
-# 	else 
-# 		puts "Result inconclusive."
-# 	end 
-# end
-
 puts 
+
 #Increments number of employee for use in until loop
 number_of_employees -= 1
 
+#Provides how many employees are left to process
 puts "There are #{number_of_employees} employees left to be processed."
-
-
 
 end 
 puts
@@ -166,9 +145,7 @@ puts "Actually, nevermind! What do these questions have to do with anything? Let
 
 
 #Still to Fix:
-# Issues with names 
-# 'No' repeating itself 
-# Almost certainly a vampire response. 
+#Allergies 
 
 
 
