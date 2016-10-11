bd48d90d0bd2efe341e2a4e7820f1dba4beae756
Ethan Fertsch <efertsch@gmail.com>
2016-10-10 19:19:22 -0700

    Reposition empty hash before start of UI

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,4 +1,6 @@
+# Create Client_Info Empty Hash 
 
+Client_Info = {}
 
 # Add UI to collect client infomation
 puts "Hello, and welcome to client services!"
@@ -8,35 +10,39 @@ puts
 
 puts "Please provide your full name:"
 client_name = gets.chomp
-p client_name 
+client_name.to_sym
+# p client_name 
 
 puts "Please enter your age:"
-client_age = gets.to_i
-p client_age 
+client_age = gets.chomp
+client_age.to_sym 
+# p client_age 
 
 puts "How many children do you have?"
-number_of_children = gets.to_i
-p number_of_children
+number_of_children = gets.chomp
+number_of_children.to_sym 
+# p number_of_children
 
 puts "What is your desired Decor Theme?"
 decor_theme = gets.chomp
-p decor_theme
+decor_theme.to_sym 
+# p decor_theme
 
 puts "What type of unit do you currently reside in?"
 unit_type = gets.chomp
-p unit_type 
+unit_type.to_sym 
+# p unit_type 
 
 puts "Do you have any pets? Please answer 'yes' or 'no'"
 pets = gets.chomp
-p pets 
+pets.to_sym 
+# p pets 
 
 puts "Thank you for your entry, now let's design your dream home!"
 
 
 
 
-# Create Client_Info Hash 
-# Add keys as symbols 
 
 Client_Info = {
 	client_name: "", 
@@ -51,7 +57,7 @@ Client_Info = {
 
 p Client_Info 
 
-# Add UI to collect client infomation
+
 # Convert user input to appropriate data type
 # Print hash when client has answered all questions 
 # Give the user the opportunity to update a key - if "none" skip 
