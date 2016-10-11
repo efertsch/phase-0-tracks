effb5082567327bd209b66ae0c7fd9bf00b4b2f2
Ethan Fertsch <efertsch@gmail.com>
2016-10-10 19:08:53 -0700

    Add UI and prompt user for input, test varified all aspects are currently functional

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,4 +1,42 @@
+
+
+# Add UI to collect client infomation
+puts "Hello, and welcome to client services!"
+puts "We look forward to working with you and your space."
+
+puts 
+
+puts "Please provide your full name:"
+client_name = gets.chomp
+p client_name 
+
+puts "Please enter your age:"
+client_age = gets.to_i
+p client_age 
+
+puts "How many children do you have?"
+number_of_children = gets.to_i
+p number_of_children
+
+puts "What is your desired Decor Theme?"
+decor_theme = gets.chomp
+p decor_theme
+
+puts "What type of unit do you currently reside in?"
+unit_type = gets.chomp
+p unit_type 
+
+puts "Do you have any pets? Please answer 'yes' or 'no'"
+pets = gets.chomp
+p pets 
+
+puts "Thank you for your entry, now let's design your dream home!"
+
+
+
+
 # Create Client_Info Hash 
+# Add keys as symbols 
 
 Client_Info = {
 	client_name: "", 
@@ -10,13 +48,9 @@ Client_Info = {
 }
 
 #Test for hash functionality with empty strings, boolean, and integer
+
 p Client_Info 
 
-# Add key/value pairs to hash with strings, integers, and booleans 
-
-
-# Keys should be symbols
-# Test to make sure hash is functional
 # Add UI to collect client infomation
 # Convert user input to appropriate data type
 # Print hash when client has answered all questions 
