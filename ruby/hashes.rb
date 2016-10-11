3dab756daad8cdfaaae5ef956e41131b1bb220b9
Ethan Fertsch <efertsch@gmail.com>
2016-10-10 19:58:43 -0700

    Successfully add code to prompt user for input and add input to hash as key/symbol and value pairs

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,6 +1,8 @@
-# Create Client_Info Empty Hash 
+# Create Client_Info Empty Hash and add keys set to empty values (strings)
 
 Client_Info = {}
+
+
 
 # Add UI to collect client infomation
 puts "Hello, and welcome to client services!"
@@ -9,51 +11,47 @@ puts "We look forward to working with you and your space."
 puts 
 
 puts "Please provide your full name:"
-client_name = gets.chomp
-client_name.to_sym
+client_name = gets.chomp 
+Client_Info[:client_name] = client_name
 # p client_name 
 
 puts "Please enter your age:"
 client_age = gets.chomp
-client_age.to_sym 
+Client_Info[:client_age] = client_age
 # p client_age 
 
 puts "How many children do you have?"
-number_of_children = gets.chomp
-number_of_children.to_sym 
+number_of_children = gets.to_i
+Client_Info[:number_of_children] = number_of_children
 # p number_of_children
 
 puts "What is your desired Decor Theme?"
 decor_theme = gets.chomp
-decor_theme.to_sym 
+Client_Info[:decor_theme] = decor_theme
 # p decor_theme
 
 puts "What type of unit do you currently reside in?"
 unit_type = gets.chomp
-unit_type.to_sym 
+Client_Info[:unit_type] = unit_type
 # p unit_type 
 
 puts "Do you have any pets? Please answer 'yes' or 'no'"
 pets = gets.chomp
-pets.to_sym 
+Client_Info[:pets] = pets 
 # p pets 
 
 puts "Thank you for your entry, now let's design your dream home!"
 
 
-
-
-
-Client_Info = {
-	client_name: "", 
-	client_age: 40,
-	number_of_children: "",
-	decor_theme: "",
-	unit_type: "", 
-	pets: true
-}
-
 #Test for hash functionality with empty strings, boolean, and integer
+# Client_Info = {
+# 	client_name: "", 
+# 	client_age: 40,
+# 	number_of_children: "",
+# 	decor_theme: "",
+# 	unit_type: "", 
+# 	pets: true
+# }
 
 p Client_Info 
 
