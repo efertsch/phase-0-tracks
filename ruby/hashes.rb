75f981ebb14328f06a60b79f0586670d07aa37d7
Ethan Fertsch <efertsch@gmail.com>
2016-10-10 18:36:50 -0700

    Initial commit, add pseudocode for program and create empty hash for client information

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+# Create Client_Info Hash 
+Client_Info = {}
+# Add key/value pairs to hash with strings, integers, and booleans 
+# Keys should be symbols
+# Test to make sure hash is functional
+# Add UI to collect client infomation
+# Convert user input to appropriate data type
+# Print hash when client has answered all questions 
+# Give the user the opportunity to update a key - if "none" skip 
+# If user wishes to make change, ask for new value and update the key
+# Print latest version of the hash 
+# Exit the program
+
