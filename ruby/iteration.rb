a5425845fcbc615d191cc05cd789276cd04acf35
Ethan Fertsch <efertsch@gmail.com>
2016-10-11 20:00:02 -0700

    Iterate over hash using .each method and add additional comments to complete Release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,11 +5,11 @@ end
 # sf_giants { |chant_1, chant_2| puts "#{chant_1}\n #{chant_2}" }
 
 
-
 #Make an array
 grocery_list = ["bananas", "chicken", "milk", "cereal"]
 new_list = []
 
+#Print before modification
 p grocery_list
 p new_list 
 
@@ -18,14 +18,16 @@ grocery_list.each do |food_item|
 new_list << food_item.upcase  
 end 
 
+#Print after modification
 p grocery_list
 p new_list
 
 #Iterate over array with .map!
+p grocery_list 
 grocery_list.map! do |food_item|
 food_item.capitalize    
 end 
-
+#Print after modification
 p grocery_list
 
 
@@ -36,6 +38,12 @@ car_hash = {
 	honda: "civic",
 	toyota: "prius"
 }
+#print hash
 p car_hash
 
 #Iterate over hash with .each 
+car_hash.each do |car, model|
+	puts "My favorite #{car} is the #{model}!"
+end 
+
+
