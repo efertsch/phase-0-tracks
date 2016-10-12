264120184c502d6ddc648f6b679c9c6b4334b57d
Ethan Fertsch <efertsch@gmail.com>
2016-10-11 19:32:42 -0700

    Add grocery list array and car make and model hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,3 +4,15 @@ def sf_giants
 end
 
 sf_giants { |chant_1, chant_2| puts "#{chant_1}\n #{chant_2}" }
+#Make an array
+grocery_list = ["bananas", "chicken", "milk", "cereal"]
+p grocery_list
+
+#Make a hash
+car_hash = {
+	ford: "focus",
+	subaru: "outback",
+	honda: "civic",
+	toyota: "prius"
+}
+p car_hash
