fd2f4d656f039658c245229cdf195f9efc61d09f
Ethan Fertsch <efertsch@gmail.com>
2016-10-13 15:40:11 -0700

    swap first and last names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,7 +7,34 @@
 # Identify consonants and create variable to store consonants in ( split -> array?)
 # Iterate through array and change all consonants to next consonants(using .next on consonants)
 
-def alias_creator(name) 
-	#something
 
+def alias_creator(name) 
+	name_as_array = name.split(" ")
+	reversed_name = name_as_array.sort {|a,b| b <=> a }
+	reversed_name.join(" ")
+	# index = 0
+	# while index < reversed_name.length
+	# 	p reversed_name[index]
+	# 	index+=1
+	# 	reversed_name.each do |letter|
+
+	# 	end 
+	# end 
 end 
+
+p alias_creator("Felicia Torres")
+
+
+
+# def vowel_finder(name)
+# 	vowels = ["a","e","i","o","u"]
+# 	index = 0
+# 	while index < name.length 
+# 		name[index] == vowels[index]
+# 		name[index] << vowels[index]
+# 		index +=1
+# 	end 
+
+# end 
+
+# p vowel_finder("Felicia Torres")
