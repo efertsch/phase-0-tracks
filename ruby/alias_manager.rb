ff04434749295bfb47920d238d31496ee853e311
Ethan Fertsch <efertsch@gmail.com>
2016-10-13 14:14:29 -0700

    Add pseudocode/algorithm to alias_manager file for Release 0

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,9 @@
+# Set up method skeleton
+# Initialize real_name variable set to string 
+# Find ruby method to swap first and last name of real_name string  
+# Create alphabet variable and set to string
+# Identify vowels (aeiou) in alphabet and create variable to store vowels in (split ->array?)
+# Iterate through array and change all vowels to next vowel (using .next on vowel)
+# Identify consonants and create variable to store consonants in ( split -> array?)
+# Iterate through array and change all consonants to next consonants(using .next on consonants)
+
