dc93de940d2feb3b145fc8014859d3720f803a05
Ethan Fertsch <efertsch@gmail.com>
2016-10-13 12:09:34 -0700

    Create barnyard hash with several components for nested data structure practice

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,49 @@
+# Determine example and set up hash
+Barnyard = {
+Barn: {
+	stalls: {
+ 		stall_1: "Horse", 
+ 		stall_2: "Pig",
+ 		stall_3: "Cow",
+ 		stall_4: "Sheep",
+	},
+
+tack_room: [
+	"saddle", 
+	"bridle", 
+	"grooming_supplies",
+	"boots"
+	], 
+},
+
+Paddock: [
+	"trough",
+	"grass",
+	"riding ring",
+	"hay bales",
+],
+
+Coop:
+	chickens: {
+		betty: "Lays many eggs",
+		barbara: "Lays some eggs",
+		blaire: "Doesn't lay eggs"
+	},
+
+	rooster: {
+		big_daddy: "Annoying between the hours of 4-7am"
+	},
+
+Silo: [
+	storage_space: [
+		"tractor",
+	 	"tools"
+	 ], 
+	main_space: [
+		"feed",
+		"grain"
+	],
+]
+
+}
+
