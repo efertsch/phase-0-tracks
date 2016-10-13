4fb4f86ea2abd67c0f45ef68af61e5ca181ba4c6
Ethan Fertsch <efertsch@gmail.com>
2016-10-13 12:41:09 -0700

    Break down stalls into individual hashes, make silo a hash instead of array, add test code to demonstrate comfortability with nested data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,12 +1,28 @@
-# Determine example and set up hash
+
 barnyard = {
 
 	barn: {
 		stalls: {
-	 		stall_1: "Horse", 
-	 		stall_2: "Pig",
-	 		stall_3: "Cow",
-	 		stall_4: "Sheep",
+	 		stall_1: {
+	 			animal_type: "Horse", 
+	 			animal_name: "Mr.Ed",
+	 			favorite_foods: ["sugar cubes", "carrots"]
+	 		},
+			stall_2: {
+				animal_type: "Pig", 
+	 			animal_name: "Wilbur",
+	 			favorite_foods: ["Literally anything", "Literally everything"]
+	 		},
+	 		stall_3: {
+	 			animal_type: "Cow", 
+	 			animal_name: "Clo",
+	 			favorite_foods: ["Grass", "Leftover cud"]
+	 			},
+	 		stall_4: {
+	 			animal_type: "Sheep", 
+	 			animal_name: "Wooly",
+	 			favorite_food: ["Whatever sheep eat"]
+	 		}
 		},
 	
 	tack_room: [
@@ -24,18 +40,25 @@ barnyard = {
 		"hay bales",
 	],
 	
-	coop:
+	coop: {
 		chickens: {
 			betty: "Lays many eggs",
 			barbara: "Lays some eggs",
 			blaire: "Doesn't lay eggs"
 		},
-	
+
+		layboxes: {
+			betty_box: 1, 
+			barbara_box: 2, 
+			blaire_box: 3, 
+		},
+
+	},
 		rooster: {
 			big_daddy: "Annoying between the hours of 4-7am"
 		},
 	
-	silo: [
+	silo: {
 		storage_space: [
 			"tractor",
 		 	"tools"
@@ -44,9 +67,22 @@ barnyard = {
 			"feed",
 			"grain"
 		],
-	]
+	}
 
 }
 
+p barnyard 
 
+p barnyard[:barn][:stalls][:stall_1][:animal_type]
+
+p barnyard[:barn][:stalls][:stall_2][:favorite_foods][1]
+
+p barnyard[:paddock]
+p barnyard[:paddock].push("manuer pile")
+
+p barnyard[:coop][:chickens][:betty]
+
+p barnyard[:silo]
+p barnyard[:silo][:storage_space][1] = "farming tools"
+p barnyard[:silo]
 
