052fa4bd3d7ed7feab2c91203b1a8584fb662184
Ethan Fertsch <efertsch@gmail.com>
2016-10-16 15:59:26 -0700

    Re-order code to separate methods and debugging/UI, add a comments describing program for ease of explanation to reviewer.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,40 +1,47 @@
+#Initial pseudocode 
 # Set up method skeleton
 # Initialize real_name variable set to string 
 # Find ruby method to swap first and last name of real_name string  
-# Create alphabet variable and set to string
-# Identify vowels (aeiou) in alphabet and create variable to store vowels in (split ->array?)
+# Create vowel variable and set to string
 # Iterate through array and change all vowels to next vowel (using .next on vowel)
 # Identify consonants and create variable to store consonants in ( split -> array?)
 # Iterate through array and change all consonants to next consonants(using .next on consonants)
 
-users_hash = {}
 
+
+#Unable to complete all aspects of challenge
+# This program takes user input, swaps the first and last name, reverses the lettering and uses swapcase method
+# Stores in user data as key/value pairs in hash 
+# Loop program until user types quit
+# Prints statement using interpolation to describe real and fake names
+
+def alias_creator(name) 
+	name_as_array = name.split(' ')
+	reversed_name = name_as_array.sort { | x,y | y <=> x }
+	swapped_name = reversed_name.map! { |name| name.swapcase.reverse }
+	joined_name = swapped_name.join(' ')
+end 
+
+users_hash = {}
 puts "Please enter your name:"
 real_name = gets.chomp
 real_name.to_sym
 while real_name != "quit" 
-		def alias_creator(name) 
-			name_as_array = name.split(' ')
-			reversed_name = name_as_array.sort { | x,y | y <=> x }
-			swapped_name = reversed_name.map! { |name| name.swapcase.reverse }
-			joined_name = swapped_name.join(' ')
-		end 
 	alias_creator(real_name)	
 	fake_name = alias_creator(real_name)
-	p "Your fake name is #{alias_creator(real_name)}!"
 	users_hash[real_name] = fake_name
 	puts "Please enter another name:"
 	real_name = gets.chomp
 end 
 
 p users_hash
+
 users_hash.each do |real, fake|
 	puts "#{real} is also known as #{fake}, can you believe it?"
 end 
 
 
 # -----------------------------------------------------------
-
 
 #All code from here down was an attempt to get the more difficult version of the assignment 
 #performed but I did not have enough time to get the code working properly. 
@@ -65,6 +72,7 @@ end
 
 # p next_consonant('m')
 # p next_consonant('g')
+
 
 #Combine alias_creator method with consonant and vowel finding methods 
 # def alias_creator(name) 
