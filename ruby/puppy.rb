ba0f479d7f798bb65611eccc1ba28164d348d4c4
Ethan Fertsch <efertsch@gmail.com>
2016-10-17 18:18:37 -0700

    Add puppy characteristics and behavior pseudocode to puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+#Puppy Class Design
+
+#Species 
+	#Cuticus Pupicus 
+
+#Characteristics 
+	# Lifespan: variable
+	# Fuzzy: yes
+	# Fur color: variable
+	# Size: small
+	# Number of legs: 4
+
+#Behavior 
+	# Lick
+	# Pee on floor
+	# Chew
+	# Run 
+	# Play
