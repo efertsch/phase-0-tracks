0c15e82341ead569bd4ab34b60db98661cfe3fe6
Ethan Fertsch <efertsch@gmail.com>
2016-10-18 19:19:07 -0700

    Add test code to investigate functionality of class methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,13 @@
-class Santa 
+class Santa
 
 	def speak(santa_greeting)
-		puts "Ho, ho, ho! Haaaaapy holidays!" 
+		puts santa_greeting
+		santa_greeting
 	end 
 
 	def eat_milk_and_cookies(cookie_type)
-		puts "That was a good #{cookie_type}!"
+		puts "That was a good #{cookie_type} cookie!"
+		cookie_type
 	end 	
 
 	def initialize
@@ -13,3 +15,12 @@ class Santa
 	end 	
 
 end 
+
+
+#TEST CODE
+ 
+santa =  Santa.new 
+santa.speak("Ho, ho, ho! Haaaaapy holidays!")
+santa.eat_milk_and_cookies("chocolate chip")
+
+
