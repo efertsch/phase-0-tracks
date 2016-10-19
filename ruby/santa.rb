15bec6b66e12410ed05e1a9ef701a66a4534be1f
Ethan Fertsch <efertsch@gmail.com>
2016-10-18 19:28:59 -0700

    update santa class with gender, ethnicity, reindeer ranking, and age attributes in initialize method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,13 @@
 class Santa
 
+	def initialize(input_gender, input_ethnicity)
+		puts "Initializing Santa instance..."
+		@gender = input_gender
+		@ethnicity = input_ethnicity
+		@reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end 
+
 	def speak(santa_greeting)
 		puts santa_greeting
 		santa_greeting
@@ -8,18 +16,14 @@ class Santa
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type} cookie!"
 		cookie_type
-	end 	
-
-	def initialize
-		puts "Initializing Santa instance..."
-	end 	
+	end 		
 
 end 
 
 
 #TEST CODE
  
-santa =  Santa.new 
+santa =  Santa.new
 santa.speak("Ho, ho, ho! Haaaaapy holidays!")
 santa.eat_milk_and_cookies("chocolate chip")
 
