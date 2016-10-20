be6a3a08cdde18089c108196ab8b1d0e57ded433
Ethan Fertsch <efertsch@gmail.com>
2016-10-20 08:50:54 -0700

    Test functionality of mixin by creating new instances of the classes and calling the module methods on the classes. complete challenge 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -7,8 +7,6 @@
 #     end
 # end
 
-
-
 # puts Shout.yell_angrily("I am frustrated")
 # puts Shout.yell_happily("I am ecstatic")
 
@@ -17,7 +15,25 @@ module Shout
       puts words + "!!!" + " :("
     end
 
-    def self.yell_happily(words)
-    words + "!!!" + " ^.^"
+    def yell_happily(words)
+      puts words + "!!!" + " ^.^"
     end
 end 
+
+
+class Sports_Fan
+	include Shout
+end 
+
+class Angry_Mom
+	include Shout 
+end 
+
+
+mom = Angry_Mom.new
+mom.yell_angrily("Quiet down")
+mom.yell_happily("Finally a night out")
+
+fan = Sports_Fan.new 
+fan.yell_angrily("Boooooooo")
+fan.yell_happily("Go team")
