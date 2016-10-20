4335f7c1dec75a70e93e1f5a47a809e9e659b6b6
Ethan Fertsch <efertsch@gmail.com>
2016-10-19 18:09:29 -0700

    Add getter and setter methods to access and modify attributes outside of Santa class, test functionality with driver code and move on to release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -18,23 +18,62 @@ class Santa
 		cookie_type
 	end 	
 
+
+	def celebrate_birthday
+		@age = age + 1
+	end
+
+	def get_mad_at(reindeer_name)
+		n = @reindeer.find_index(reindeer_name)
+		@reindeer.insert(8, @reindeer.delete_at(n))
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end 
+
+	def age
+		@age
+	end 
+
+	def ethnicity
+		@ethnicity
+	end 
+
+
 end 
 
 
-#TEST CODE
 
-# santa =  Santa.new 
-# santa.speak("Ho, ho, ho! Haaaaapy holidays!")
-# santa.eat_milk_and_cookies("chocolate chip")
+
+santa =  Santa.new 
+santa.speak("Ho, ho, ho! Haaaaapy holidays!")
+santa.eat_milk_and_cookies("chocolate chip")
 
 santas = []
 
 potential_genders = ["transgender", "cisgender", "agender", "gender queer", "prefer not to say"]
 potential_ethnicities = ["white", "black", "hispanic", "asain", "prefer not to say"]
   
-potential_genders.each do |i|
-	santas << Santa.new(potential_genders, potential_ethnicities)
+potential_genders.length.times do |i|
+	santas << Santa.new(potential_genders[i], potential_ethnicities[i])
 end 
 
 
-puts santas
+p santas
+
+santas = Santa.new("cisgender", "black")
+p santas
+
+p santas.celebrate_birthday
+p santas 
+
+santas.get_mad_at("Rudolph")
+p santas
+
+santas.gender = "agender"
+p santas
+
+p santas.age
+p santas.ethnicity
+
