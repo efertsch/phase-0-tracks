d2a909d3167a2e69f451714dbecff3a6ea39b76b
Ethan Fertsch <efertsch@gmail.com>
2016-10-20 10:29:28 -0700

    Add pseudocode to game file

---
+++ ruby/ruby_game/game.rb
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#PSEUDOCODE
+# Create a word game class
+# Define class and initialize method within the class that take input as parameters
+# Assign input to attributes within class
+# Print input/attributes as debugging step
+# Write a method to limit the number of guesses based on the length of the input word 
+# Store guesses in some way
+# Exclude guesses that have been stored and print a repeat message
+# Write a method to provide feedback to the user
+# The method should match the length of the word and to a string of underscores (hangman style)
+# If the guess input is equal to a character in the input word, replace the underscore at that position with the guessed character
+# print out the underscore string to the user 
+# If the user wins print “Winner Winner Chicken Dinner! Play again” otherwise, print “Better luck next time sucker!”
