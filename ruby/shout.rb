1c8833e100d0e2de9fcc43dd612764602a345de8
Ethan Fertsch <efertsch@gmail.com>
2016-10-20 08:43:06 -0700

    Add shout module  to file and create two new classes that include the shout module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,23 @@
-module Shout
-  def self.yell_angrily(words)
-      words + "!!!" + " :("
+# module Shout
+#   def self.yell_angrily(words)
+#       words + "!!!" + " :("
+#     end
+#   def self.yell_happily(words)
+#     words + "!!!" + " ^.^"
+#     end
+# end
+
+
+
+# puts Shout.yell_angrily("I am frustrated")
+# puts Shout.yell_happily("I am ecstatic")
+
+module Shout 
+	def yell_angrily(words)
+      puts words + "!!!" + " :("
     end
-  def self.yell_happily(words)
+
+    def self.yell_happily(words)
     words + "!!!" + " ^.^"
     end
-end
-
-
-
-puts Shout.yell_angrily("I am frustrated")
-puts Shout.yell_happily("I am ecstatic")
+end 
