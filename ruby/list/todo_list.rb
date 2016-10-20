b51448466676531cabb1fbbd0ba309a65a4f92bd
Ethan Fertsch <efertsch@gmail.com>
2016-10-20 15:21:02 -0700

    Add last bit of code to pass last test and complete challenge

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,18 +1,24 @@
 class TodoList
+
   def initialize(input_list)
     @list = input_list
   end
+
   def get_items
     @list
   end
+
   def add_item(item_to_add)
     @list << item_to_add
   end
+
   def delete_item(item_to_delete)
     n = @list.index(item_to_delete)
     @list.delete_at(n)
   end
-  def retrieve_item_by_index(x)
-    @list.fetch(x)
+
+  def get_item(x)
+    @list[x]
   end
+
 end
