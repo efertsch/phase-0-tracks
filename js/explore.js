06402f30144e485f091a215c99db369dab845354
Ethan Fertsch <efertsch@gmail.com>
2016-10-24 12:20:41 -0700

    Initial commit for explore file, add psuedocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+//PSUEDOCODE 
+// Write a reverse function that takes a string as a parameter
+// Define a variable to reverse and set it equal to a string 
+// Within the function loop through each letter in the string 
+// Set a counter variable 
+// For each letter in the string, while the counter is less than the length of the string 
+// Find the letter in the last position and move it forward in the string one postition 
+// Print the result to the console
