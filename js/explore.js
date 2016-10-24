49e778c41f6d1cb071514f8d4e7bd5a593d6c5b9
Ethan Fertsch <efertsch@gmail.com>
2016-10-24 13:24:08 -0700

    Adjust pseudocode and add function to reverse a string by backward incrementation

--- js/explore.js
+++ js/explore.js
@@ -1,8 +1,19 @@
 //PSUEDOCODE 
 // Write a reverse function that takes a string as a parameter
-// Define a variable to reverse and set it equal to a string 
-// Within the function loop through each letter in the string 
-// Set a counter variable 
-// For each letter in the string, while the counter is less than the length of the string 
-// Find the letter in the last position and move it forward in the string one postition 
-// Print the result to the console
+// Define a variable to reverse and set it equal to an empty string 
+// Within the function loop through each letter in the original string 
+// Find the letter in the last position of the string
+// For each letter in the string, while the length of the string is greater than or equal to zero, increment down by 1 
+// Store the letter at each index (again, incrememnting down) in the empty string variable
+// Return the new string
+// Call the function with a sample string 
+
+var StringReverser = function(string) {
+	var ReversedString = " ";
+	for (var i = string.length -1 ; i >= 0; i--) {
+		ReversedString +=  string[i];
+	};
+	 return ReversedString;
+}
+
+console.log(StringReverser("hello"));
