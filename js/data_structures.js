88d87380f21731408f16afdfd16301b2c07ceec0
Ethan Fertsch <efertsch@gmail.com>
2016-10-25 12:48:59 -0700

    complete release 2, write loop to go through arrays and add key values to object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,17 @@
-var colors = ["green", "magenta", "yellow", "blue"];
-var names = ["Bob", "Steve", "Michael", "David"];
+var horseColors = ["green", "magenta", "yellow", "blue"];
+var horseNames = ["Bob", "Steve", "Michael", "David"];
+
+horseColors.push("puce");
+horseNames.push("Big Tony");
+
+console.log(horseColors, horseNames);
+
+
+var horses = {};
+
+	for (var i = 0; i < horseNames.length; i++) {
+		horses[horseNames[i]] = horseColors[i]
+}
+
+
+console.log(horses);
