135c9e1f8fa9a5f1f4349a46da96d261cf9e1a59
Ethan Fertsch <efertsch@gmail.com>
2016-10-25 16:16:02 -0700

    Add pseudocode to algos file to prep for remainder of releases

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,4 @@
-//Release 1: PSEUDOCODE
+//Release 0: PSEUDOCODE
 // Write a function that accepts an array of words as a parameter
 	// Within the function write a for loop that:
 		// Initializes a counter variable and sets it equal to 0
@@ -15,23 +15,39 @@
 // Call the method using multiple arrays to test 
 
 
-
-// var LengthFinder = function(array) {
-// 	var longestWord = array[0];
-// 	for (var i = 0; i <= array.length; i++) {
-// 		stringLength = array[i].length;
-// 		if (stringLength > longestWord.length) {
-// 			var longestWord = array[i];
-// 			console.log(longestWord);
-// 		}
+ 
+var LengthFinder = function(array) {
+	var longestWord = array[0];
+	for (var i = 0; i <= array.length; i++) {
+		stringLength = array[i].length;
+		if (stringLength > longestWord.length) {
+			var longestWord = array[i];
+			console.log(longestWord);
+		}
 		
-// 	}
-// };
+	}
+};
 
-// var testArray = ["long phrase","longest phrase","longer phrase"];
-// LengthFinder(testArray);
+var testArray = ["long phrase","longest phrase","longer phrase"];
+LengthFinder(testArray);
+
+// Release 1: PSEUDOCODE
+// Write a function that accepts two objects as a parameter
+	// Within the function, write a loop that:
+		// Initializes a counter variable and sets it equal to 0
+		// Compares the length of the array to the counter variable 
+		// Increments by 1 to determine when the loop stops 
+			// Return the values of the first object at each index 
+			// If the values from the first object are equal to any values in the second object
+			// Reutrn the like objects, print a statement with like objects
+			// Otherwise don't return anything, print a statement saying there are no like objects 
+// Add driver code to call the function 
 
 
+// Release 2: PSEUDOCODE
+// Write a function that accepts an integer as a parameter to return an array of strings based on the integer
+// Create a variable and set it to an empty array
+// Use a random number generator to generate the length of each word
 
 
 
