5b6cb0fb632f801418d3ae4a59d1954d799da622
Ethan Fertsch <efertsch@gmail.com>
2016-10-25 14:31:05 -0700

    Add psuedocode for release 1 to file

---
+++ js/algos.js
@@ -0,0 +1,17 @@
+//Release 1: PSEUDOCODE
+// Write a function that accepts an array of words as a parameter
+	// Within the function write a for loop that:
+		// Initializes a counter variable and sets it equal to 0
+		// Compares the length of the array to the counter variable 
+		// Increments by 1 to determine when the loop stops
+	// Within the loop:
+		// Find the length of each string item in the array
+		// For each item, return the length of the string  
+		// If the length of the string is greater than the length of the string before it
+			// Return the string 
+		// Otherwise, if the length of the string is shorter than the one before it
+			// Move to the next item 
+	// Return the string with the greatest length 
+// Call the method using multiple arrays to test 
+
+
