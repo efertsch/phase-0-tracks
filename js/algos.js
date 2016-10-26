5f2402cd417200c2d75f20be7db53fa642f18b8f
Ethan Fertsch <efertsch@gmail.com>
2016-10-26 13:02:40 -0700

    Add code to start working on release 2, cannot get release 1 to work properly

--- js/algos.js
+++ js/algos.js
@@ -13,47 +13,49 @@ var LengthFinder = function(array) {
 
 var matchKeyValuePairs = function(object1, object2) {
 	for (var key in object1) {
-		// var object1Keys = object1.key
-		console.log(object1.key);
-	}
-	for (var key in object2) {
-		var object2Keys = object2.key
-		console.log(object2Keys);
-	}
-	// if (object2Keys == object1Keys) {
-	// 	return true; 
-	// }
-	// else {
-	// 	return false; 
-	// }
+		var object1Keys = object1[key];
+		var object2Keys = object2[key];
+		// console.log(object1Keys);
+		// console.log(object2Keys);
+		do
+			return true;
+		while (object1Keys === object2Keys);
+}
 }
 
-var object1 = {name: "Steven", age: 54};
-var object2 = {name: "Tamir", age: 54};
+var generateRandString = function(number) {
+		number.times; 
+		var letters = "abcdefghijklmnopqrstuvwxyz";
+		var lettersLength = letters.length
+		var result = "";
+		var stringArray = [];
+		for (var i = 0; i < lettersLength; i++) {
+			var randString = Math.random() * letters.length;
+			result += letters.substr(randString, 10);
+			stringArray.push(result);
+		}
 
-matchKeyValuePairs(object1, object2);
-
-
-
-
-
-
-
-
-
-
-
-
+	return stringArray;
+}
 
 
 
+var testArray = ["long phrase","longest phrase","longer phrase"];
+var testArray2 = ["toad", "volcano", "everglade", "bibliography"];
+LengthFinder(testArray);
+LengthFinder(testArray2);
+
+var Man1 = {name: "Steven", age: 54};
+var Man2 = {name: "Tamir", age: 54};
+
+matchKeyValuePairs(Man1, Man2);
+
+generateRandString(3);
 
 
 
-// var testArray = ["long phrase","longest phrase","longer phrase"];
-// var testArray2 = ["toad", "volcano", "everglade", "bibliography"];
-// LengthFinder(testArray);
-// LengthFinder(testArray2);
+
+
 
 
 //Release 0: PSEUDOCODE
@@ -86,9 +88,20 @@ matchKeyValuePairs(object1, object2);
 
 
 // Release 2: PSEUDOCODE
-// Write a function that accepts an integer as a parameter to return an array of strings based on the integer
+// Write a function that accepts an integer as a parameter 
+// Output should be an array of random strings of varying lengths 
 // Create a variable and set it to an empty array
-// Use a random number generator to generate the length of each word
+// For each instance of the number parameter, create a random word
+// For the random word, generate a random length with a range of 1 - 10 letters
+// Add the word to the array 
+// Return the array
+// Call the function 
+
+
+
+
+
+
 
 
 
