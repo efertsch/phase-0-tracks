9da217e098cfd89a89cb3db5c4182536371208d6
Ethan Fertsch <efertsch@gmail.com>
2016-10-25 21:58:02 -0700

    Add code to start release 1

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,61 @@
+var LengthFinder = function(array) {
+	var longestWordLength = 0;
+	var longestWord = "";
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].length > longestWordLength) {
+			longestWordLength = array[i].length;
+			longestWord = array[i];
+		}
+	}
+	console.log(longestWord);
+};
+
+
+var matchKeyValuePairs = function(object1, object2) {
+	for (var key in object1) {
+		// var object1Keys = object1.key
+		console.log(object1.key);
+	}
+	for (var key in object2) {
+		var object2Keys = object2.key
+		console.log(object2Keys);
+	}
+	// if (object2Keys == object1Keys) {
+	// 	return true; 
+	// }
+	// else {
+	// 	return false; 
+	// }
+}
+
+var object1 = {name: "Steven", age: 54};
+var object2 = {name: "Tamir", age: 54};
+
+matchKeyValuePairs(object1, object2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var testArray = ["long phrase","longest phrase","longer phrase"];
+// var testArray2 = ["toad", "volcano", "everglade", "bibliography"];
+// LengthFinder(testArray);
+// LengthFinder(testArray2);
+
+
 //Release 0: PSEUDOCODE
 // Write a function that accepts an array of words as a parameter
 	// Within the function write a for loop that:
@@ -13,28 +71,6 @@
 			// Move to the next item 
 	// Return the string with the greatest length 
 // Call the method using multiple arrays to test 
-
-
- 
-var LengthFinder = function(array) {
-	var longestWordLength = 0;
-	var longestWord = "";
-	for (var i = 0; i < array.length; i++) {
-		if (array[i].length > longestWordLength) {
-			longestWordLength = array[i].length;
-			longestWord = array[i];
-		}
-	}
-	console.log(longestWord);
-};
-
-var testArray = ["long phrase","longest phrase","longer phrase"];
-var testArray2 = ["toad", "volcano", "everglade", "bibliography"];
-LengthFinder(testArray);
-LengthFinder(testArray2);
-
-
-
 
 // Release 1: PSEUDOCODE
 // Write a function that accepts two objects as a parameter
