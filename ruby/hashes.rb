85a46bf74aecdd58fe5f0673b86c831ffb73c704
Ethan Fertsch <efertsch@gmail.com>
2016-11-02 14:26:52 -0700

    Add psuedocode to file and begin building user interface

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,1 +1,47 @@
+client_information = {}
 
+puts "Please provide your full name:"
+client_name = gets.chomp
+client_information[:name] = client_name 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# PSEUDOCODE
+# Create an empty hash 
+# Add UI to prompt for details about a given client
+# Store the input in a variable
+# Assign the variable to a key in the hash
+# Add the client information to the hash 
+# Print the hash to the screen once the user has answered all questions
+# Provide the user the opporunity to update a key 
+# IF the user answer is "none", skip that section
+# ELSE update the key to the new value 
+# Print the new hash and exit the program 
