e6e7d0cb4eeb15b90307b982cbd2b5bf06a76ac6
Ethan Fertsch <efertsch@gmail.com>
2016-11-02 16:31:37 -0700

    Create nested data structure for classic rock band of the 1970s, including band members, albums, and tour dates/locations

---
+++ ruby/nested_data_structures_v2.rb
@@ -0,0 +1,41 @@
+classic_rock = {
+	led_zeppelin = {
+		members = {
+			"Robert Plant" <= "Vocals",
+			"Jimmy Page" <= "Guitar",
+			"John Paul Jones" <= "Bass",
+			"John Bonham" <= "Drums"
+		},
+		albums: ["Physical Graffiti", "Houses of the Holy", "Led Zeppelin IV"],
+		tour: {
+			March_1970: ["Portland", "Denver", "Houston", "Philidelphia"],
+			April_1970: [ "Boston", "Miami Beach", "St.Louis", "Montreal"]
+		},
+	},
+	rolling_stones = {
+		members = {
+			"Kieth Richards" <= "Vocals/Piano",
+			"Mick Jagger" <= "Guitar",
+			"Mick Taylor" <= "Guitar",
+			"Bill Wyman" <= "Bass",
+			"Charlie Watts" <= "Drums"
+		albums: ["Sticky Fingers", "Exile on Main St.", "Some Girls"],
+		tour: {
+			September_1970: ["Stockholm", "Paris", "Berlin", "Rome"],
+			October_1970: [ "Milan", "Amsterdam", "Lyon", "Frankfurt"],
+	},
+
+	rolling_stones = {
+		members = {
+			"Kieth Richards" <= "Vocals/Piano",
+			"Mick Jagger" <= "Guitar",
+			"Mick Taylor" <= "Guitar",
+			"Bill Wyman" <= "Bass",
+			"Charlie Watts" <= "Drums"
+		albums: ["Sticky Fingers", "Exile on Main St.", "Some Girls"],
+		tour: {
+			September_1970: ["Stockholm", "Paris", "Berlin", "Rome"],
+			October_1970: [ "Milan", "Amsterdam", "Lyon", "Frankfurt"],
+
+
+}
