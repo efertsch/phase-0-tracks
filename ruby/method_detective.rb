a36962d85bb4354901a95a85e9ea5c3b884d9840
Ethan Fertsch <efertsch@gmail.com>
2016-11-02 13:42:57 -0700

    Finish adding methods to complete challenge 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -21,27 +21,27 @@ p "the usual".concat(" suspects")
 p "the usual" << "suspects"
 #=> "the usual suspects"
 
-p " suspects"
+p " suspects".prepend("the usual")
 # => "the usual suspects"
 
-p "The case of the disappearing last letter"
+p "The case of the disappearing last letter".chop
 #OR
-p "The case of the disappearing last letter"
+p "The case of the disappearing last letter".slice(0..38)
 # => "The case of the disappearing last lette"
 
-p "The mystery of the missing first letter"
+p "The mystery of the missing first letter".delete("T")
 #OR
-p "The mystery of the missing first letter"
+p "The mystery of the missing first letter".slice(1..38)
 # => "he mystery of the missing first letter"
 
-p "Elementary,    my   dear        Watson!"
+p "Elementary,    my   dear        Watson!".squeeze 
 # => "Elementary, my dear Watson!"
 
-p "z"
+p "z".ord 
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 # Refers to American Standard Code for Information Interchage (ASCII) and is a character encoding standard in which each character is assigned a number from 0 - 127.
 # Spaces between characters also have number values and can be determined using escape sequence: ?/
 
-p "How many times does the letter 'a' appear in this string?"
+p "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
