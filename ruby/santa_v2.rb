52378557a2e6f6744939ecd3d0e5984210a4d9df
Ethan Fertsch <efertsch@gmail.com>
2016-11-08 13:55:17 -0800

    Add three setter and two getter methods to Santa class, get made at method in progress

--- ruby/santa_v2.rb
+++ ruby/santa_v2.rb
@@ -16,6 +16,29 @@ class Santa
 		puts "That was a good #{cookie_type} cookie!"
 	end
 
+	#SETTERS
+	def celebrate_birthday
+		@age + 1 
+	end
+
+	def get_mad_at(reindeer_name)
+		reindeer_position = reindeer_ranking[reindeer_name]
+		p reindeer_position
+	end 
+
+	def gender=(new_gender) 
+		@gender = new_gender 
+	end
+
+	#GETTERS
+	def age
+		@age 
+	end
+
+	def ethinicity
+		@ethnicity
+	end   
+
 end 
 
  
@@ -26,16 +49,18 @@ end
 # Saint_Nick.speak
 # Saint_Nick.eat_milk_and_cookies("chocolate chip")
 
-diverse_santas = []
+# diverse_santas = []
 
-genders = ["transgender", "agender", "female", "male", "female", "gender fluid", "N/A"]
-ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# genders = ["transgender", "agender", "female", "male", "female", "gender fluid", "N/A"]
+# ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
-genders.length.times do |item|
-	diverse_santas << Santa.new(genders[item], ethnicities[item])
-end 
+# genders.length.times do |item|
+# 	diverse_santas << Santa.new(genders[item], ethnicities[item])
+# end 
 
-p diverse_santas
+# p diverse_santas
+
+
 
 
 
