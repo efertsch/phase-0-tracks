bc1f46039c2352f5b6dfbb807d097f1cf410e1ce
Ethan Fertsch <efertsch@gmail.com>
2016-11-07 19:52:06 -0800

    Add driver code to test instance method functionality

--- ruby/santa_v2.rb
+++ ruby/santa_v2.rb
@@ -9,8 +9,25 @@ class Santa
 	end 
 	
 	def eat_milk_and_cookies(cookie_type)
-		"That was a good #{cookie_type}!"
+		puts "That was a good #{cookie_type} cookie!"
 	end
 
 end 
+
+# TEST CODE 
+
+
+Saint_Nick = Santa.new 
+
+Saint_Nick.speak
+Saint_Nick.eat_milk_and_cookies("chocolate chip")
+
+
+
+
+
+
+
+
+
 
