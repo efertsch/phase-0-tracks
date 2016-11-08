8259838803e1920bc3fbfdb0b1ca5cd541eed9b9
Ethan Fertsch <efertsch@gmail.com>
2016-11-07 19:14:11 -0800

    Comment in puppy class

---
+++ ruby/puppy_v2.rb
@@ -0,0 +1,24 @@
+### The Puppy Class ###
+
+
+# SPECIES -------------------
+
+# Canus lupus familiaris 
+
+
+# CHARACTERISTICS -----------
+
+# Fur length: variable
+# Leg count: 4
+# Fur color: variable
+# Wet nose: true 
+
+
+# BEHAVIOR ------------------
+
+# Chew
+# Play
+# Sleep
+# Poop
+
+
