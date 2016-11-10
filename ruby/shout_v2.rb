dd44037ca6a8aa0ad2286e24af1f1a8b0fa42835
Ethan Fertsch <efertsch@gmail.com>
2016-11-09 17:27:20 -0800

    Add yelling happily method to standalone module and add driver code to test

--- ruby/shout_v2.rb
+++ ruby/shout_v2.rb
@@ -1,3 +1,14 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+
+   def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    words.upcase + "!!!" + " :)"
+  end
+
 end
+
+puts Shout.yell_angrily("Trump is President")
+puts Shout.yell_happily("The election is over")
