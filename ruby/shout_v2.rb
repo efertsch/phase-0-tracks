e065879113d60708da7beb6c95cc7df5bf9cfaa9
Ethan Fertsch <efertsch@gmail.com>
2016-11-09 17:36:20 -0800

    Convert standalone module to mixin module

--- ruby/shout_v2.rb
+++ ruby/shout_v2.rb
@@ -1,14 +1,34 @@
+# module Shout
+
+#    def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words.upcase + "!!!" + " :)"
+#   end
+
+# end
+
+# puts Shout.yell_angrily("Trump is President")
+# puts Shout.yell_happily("The election is over")
+
 module Shout
+	def yell_loudly(words)
+		puts words.upcase + "!!!"
+	end 
+end 
 
-   def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+class SportsFan 
+	include Shout
+end 
 
-  def self.yell_happily(words)
-    words.upcase + "!!!" + " :)"
-  end
+class Protestors
+	include Shout 
+end 
 
-end
+fan = SportsFan.new
+fan.yell_loudly("Go, fight, win!")
 
-puts Shout.yell_angrily("Trump is President")
-puts Shout.yell_happily("The election is over")
+protestor = Protestors.new
+protestor.yell_loudly("What do we want??? Equality! When do we want it??? Now")
