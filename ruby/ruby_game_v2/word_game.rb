a0ccdf9bea39c4657a95ae5e24f7ec5f61a78a01
Ethan Fertsch <efertsch@gmail.com>
2016-11-11 10:03:13 -0800

    Add psuedocode to word game file and declare a game class, add initialize method.

---
+++ ruby/ruby_game_v2/word_game.rb
@@ -0,0 +1,39 @@
+class Game
+
+	def initialize
+
+	end 
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# PSEUDOCODE 
+
+# Define a word game class
+# Add attributes that:
+# 	- describe game status - reader
+# 	- number of guesses - reader
+# Add methods that:
+# 	- build dashes arrays for words entered
+# 	- determine whether or not game is over
+# 	- searches an entered word for a guessed letter
+# 		- if the letter is present, replace the word at the index of the dash it exists at in the 		entered word 
+# 	- prints feedback  
+# Add driver code that includes:
+# 	- Tests method functionality
+# 	- User Interface for word and character entry 
+# 	- prints a message type based on whether the game is won or lost
