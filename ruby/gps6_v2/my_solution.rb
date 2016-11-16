a13ffb7640fdb2add755b669e77f53a917936ee1
Ethan Fertsch <efertsch@gmail.com>
2016-11-15 19:54:34 -0800

    Refactor predicted deaths method to be more DRY

--- ruby/gps6_v2/my_solution.rb
+++ ruby/gps6_v2/my_solution.rb
@@ -17,12 +17,15 @@ class VirusPredictor
     @population_density = population_density
   end
 
+
   # Is passed information from predicted deaths and speed of spread instance methods 
   def virus_effects
     predicted_deaths
     speed_of_spread
   end
 
+  # Easier to refactor 
+  # More control 
   private
 
   # Calculates predicted deaths based on population density and population per state 
@@ -30,17 +33,17 @@ class VirusPredictor
   def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      factor = 0.4 
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      factor = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      factor = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      factor = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      factor = 0.05
     end
-
+    number_of_deaths = (@population * factor).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
