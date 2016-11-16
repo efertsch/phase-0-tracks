022d6bd57fabfa4c39ded3f3d062094300431d06
Ethan Fertsch <efertsch@gmail.com>
2016-11-16 15:47:22 -0800

    first attempt at function structure and variable naming

--- js/explore_v2.js
+++ js/explore_v2.js
@@ -1,5 +1,28 @@
+var ReverseString = function(string) {
+	reversedString = ""
+	for (var i = string[-1]; i < string.length; i--) {
+		reversedString += i;
+	};
+}
+
+console.log(ReverseString("hello"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // PSEDUOCODE:
-	// Write a method that accepts a string as a parameter
+	// Write a function that accepts a string as a parameter
 	// Declare a variable and set it equal to an empty string
 	// Loop through the string beginning at the last letter, incrementing down
 	// Feed each character back into the new string
