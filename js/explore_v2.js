13e4739474f4f477685bce23bc695d57dd289801
Ethan Fertsch <efertsch@gmail.com>
2016-11-16 15:52:47 -0800

    Organize code to keep pseudocde above function and driver codes as suggested

--- js/explore_v2.js
+++ js/explore_v2.js
@@ -1,3 +1,14 @@
+
+// PSEDUOCODE:
+	// Write a function that accepts a string as a parameter
+	// Declare a variable and set it equal to an empty string
+	// Loop through the string beginning at the last letter, incrementing down
+	// Feed each character back into the new string
+	// Call the method with various strings in test code 
+
+
+
+
 var reverseString = function reverse(string) {
 	reversedString = "";
 	for (var i = string.length - 1; i >= 0; i--) {
@@ -10,22 +21,3 @@ console.log(reverseString("hello"));
 console.log(reverseString("pirates"));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// PSEDUOCODE:
-	// Write a function that accepts a string as a parameter
-	// Declare a variable and set it equal to an empty string
-	// Loop through the string beginning at the last letter, incrementing down
-	// Feed each character back into the new string
-	// Call the method with various strings in test code 
