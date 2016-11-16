78499a30120913607dc670a6889a28e888d15a95
Ethan Fertsch <efertsch@gmail.com>
2016-11-16 15:28:48 -0800

    Add pseudocode for reverse string function

---
+++ js/explore_v2.js
@@ -0,0 +1,6 @@
+// PSEDUOCODE:
+	// Write a method that accepts a string as a parameter
+	// Declare a variable and set it equal to an empty string
+	// Loop through the string beginning at the last letter, incrementing down
+	// Feed each character back into the new string
+	// Call the method with various strings in test code 
