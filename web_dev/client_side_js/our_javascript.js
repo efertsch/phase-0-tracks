c75bc3400e0343c286976bd7eb691a5a0c532f7f
Ethan Fertsch <efertsch@gmail.com>
2016-11-29 13:19:30 -0800

    Add js code with function to change the color of any list element

--- web_dev/client_side_js/our_javascript.js
+++ web_dev/client_side_js/our_javascript.js
@@ -1,2 +1,18 @@
-console.log("Our script is linked!");
+//console.log("Our script is linked!");
 
+var list_elements = document.getElementsByTagName("li");
+console.log(list_elements);
+
+
+var first_element = list_elements[0];
+var second_element = list_elements[1];
+var third_element = list_elements[2];
+
+function changeFontColor(color, list_element) {
+	var new_color = list_element.style.color = color;
+	console.log(new_color);
+}
+
+changeFontColor("blue", first_element);
+changeFontColor("purple", second_element);
+changeFontColor("green", third_element);
